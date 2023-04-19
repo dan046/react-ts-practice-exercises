744f6d123e5a90d9187cc104392627b380386e64
dan046 <38634394+dan046@users.noreply.github.com>
2023-04-19 15:57:37 +0800

    1 file modified

--- components/ArrayFilter.tsx
+++ components/ArrayFilter.tsx
@@ -1,12 +1,14 @@
 import React = require('react');
 
 export default function ArrayFilter() {
+  // Given an array of objects, use Array.filter() to create a new array with objects that have a specific property value.
   const users = [
     { id: 1, name: 'John', age: 25 },
     { id: 2, name: 'Jane', age: 30 },
     { id: 3, name: 'Bob', age: 35 },
     { id: 4, name: 'Alice', age: 40 },
   ];
+  // Filter the array to create a new array with users whose age is greater than or equal to 35.
 
   return (
     <div>
