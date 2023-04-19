8bb613f86e2855ab0a7d14e1174fe7c715cf767c
dan046 <38634394+dan046@users.noreply.github.com>
2023-04-19 15:56:58 +0800

    1 file added

---
+++ components/ArrayFilter.tsx
@@ -0,0 +1,16 @@
+import React = require('react');
+
+export default function ArrayFilter() {
+  const users = [
+    { id: 1, name: 'John', age: 25 },
+    { id: 2, name: 'Jane', age: 30 },
+    { id: 3, name: 'Bob', age: 35 },
+    { id: 4, name: 'Alice', age: 40 },
+  ];
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
